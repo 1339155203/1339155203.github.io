54732b1cbbb0c0fa89d0242fe288bb09a38c0626
wjr <1339155203@qq.com>
2022-01-20 23:18:51 +0800

    2022/1/20/23:18

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { ProjectListScreen } from "screens/project-list";
+import { AuthenticatedApp } from "authenticated-app";
+import { UnauthenticatedApp } from "unauthenticated-app";
 import "./App.css";
-import { LoginScreen } from "screens/login";
+import { useAuth } from "context/auth-context";
 function App() {
+  //根据有没有user来判断是显示（登陆注册页面）还是展示数据页面
+  //因为login和register中会自动setUser，而logout会setUser（null）
+  const { user } = useAuth();
   return (
     <div className="App">
-      {/* <ProjectListScreen /> */}
-      <LoginScreen />
+      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
     </div>
   );
 }
--- src/unauthenticated-app/register.tsx
+++ src/unauthenticated-app/register.tsx
@@ -1,26 +1,18 @@
 import React, { FormEvent } from "react";
 import { useAuth } from "context/auth-context";
 
-export const LoginScreen = () => {
-  const { login, user } = useAuth();
+export const RegisterScreen = () => {
+  const { register } = useAuth();
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    //方法1
     const username = (event.currentTarget.elements[0] as HTMLFormElement).value;
     const password = (event.currentTarget.elements[1] as HTMLFormElement).value;
-    console.log(username, password);
-    login({ username, password });
+    register({ username, password });
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      {user ? (
-        <div>
-          登陆成功,用户名：{user?.name}
-          token:{user?.token}
-        </div>
-      ) : null}
       <div>
         <label htmlFor="username">用户名</label>
         <input type="text" id={"username"} />
@@ -29,7 +21,7 @@ export const LoginScreen = () => {
         <label htmlFor="password">密码</label>
         <input type="password" id={"password"} />
       </div>
-      <button type={"submit"}>登陆</button>
+      <button type={"submit"}>注册</button>
     </form>
   );
 };
