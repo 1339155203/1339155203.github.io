3278371ae0a762a30a15d4fa0f885752f6cd1ae3
wjr <1339155203@qq.com>
2022-02-24 18:19:49 +0800

    Updates

--- src/screens/Epic/util.ts
+++
@@ -1,5 +0,0 @@
-import { useProjectIdInUrl } from "screens/Kanban/util";
-
-export const useEpicSearchParams = () => ({ projectId: useProjectIdInUrl() });
-
-export const useEpicsQueryKey = () => ["epics", useEpicSearchParams()];
--- src/utils/task-type.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "react-query";
-import { useHttp } from "./http";
-import { TaskType } from "types/Task-type";
-//用于获取任务是bug还是task
-export const useTaskTypes = () => {
-  const client = useHttp();
-
-  return useQuery<TaskType[]>(["taskTypes"], () => client("taskTypes"));
-};
